d14f3eb32181859f29a4895f3e61d1ba6a251f7f
anudeeps28 <anudeepsharma28@gmail.com>
2025-05-16 16:43:02 -0400

    updating comments for better readibility

--- 6-JavaScript/JavaScript-5/03-random-integers.js
+++ 6-JavaScript/JavaScript-5/03-random-integers.js
@@ -4,5 +4,5 @@ let step2 = step1*10; // because we want numbers from 1->10
 let step3 = Math.floor.step2; // till this step, we will always get numbers from 1->9
 let step4 = step3 + 1;
 
-// bringing it al together
+// bringing it all together
 console.log(Math.floor(Math.random() * 10) + 1);
